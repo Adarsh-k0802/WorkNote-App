3d010ad3750e48e5a562acb569f5f835054f65b1
Adarsh Kumar <adarshk0802@gmail.com>
2021-12-06 18:22:23 +0530

    Login endpoints added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -61,4 +61,50 @@ router.post(
   }
 );
 
+//Authenticate a user using POST "/api/auth/login". 
+router.post(
+    "/login",
+    [
+     
+      body("email", "Enter a valid Email").isEmail(),
+      body("password", "Password can't be blank").exists(),
+      
+    ],
+    async (req, res) => {
+        //if there are errors return bad request and the errors.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const{email,password}= req.body;
+        try{
+             let user = await User.findOne({email});
+
+             if(!user){
+                return res.status(400).json({ errors: "Please try to login with correct credentials" });
+             }
+             const passwordCompare = await bcrypt.compare(password,user.password);
+             if(!passwordCompare){
+                return res.status(400).json({ errors: "Please try to login with correct credentials" }); 
+             }
+
+             const data = {
+       
+                user:{
+                    id:user.id
+                }
+        
+              }
+              const authtoken = jwt.sign(data, JWT_SECRET);
+
+              res.json({authtoken:authtoken});
+             
+        }
+        catch (error){
+            console.error(error.message);
+      res.status(500).send("Internal Server Error Occured");
+
+        }
+    })
+
 module.exports = router;
