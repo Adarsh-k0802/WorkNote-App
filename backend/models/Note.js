c1e7308ae1be02f3f0806432d9d8b0c9e69756a4
Adarsh Kumar <adarshk0802@gmail.com>
2021-12-08 19:38:01 +0530

    Adding and Fetching Notes

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+
+  user:{
+
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+
+  },
     
   title:{
       type:String,
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,59 @@
-const express = require('express');
-
-
+const express = require("express");
 const router = express.Router();
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
+const fetchuser = require("../middleware/fetchuser");
 
-//create a user using POST "/api/auth". Doen't require Auth
-router.get('/',(req,res)=>{
-    console.log(req.body);
-    res.send("hello");
-})
+//ROUTE 1 Get all the notes using: GET "/api/auth/createuser". login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+  res.json(notes);
+    } catch (error){
+        console.error(error.message);
+  res.status(500).send("Internal Server Error Occured");
 
-module.exports = router
+    }
+  
+});
+
+//ROUTE 2 Add a new Note using: POST "/api/auth/addnote". login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid Title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 Characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+
+    try {
+        const {title, description, tag}=req.body;
+
+    //if there are errors return bad request and the errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const note = new Note({
+        title, description, tag, user:req.user.id
+    })
+   const savedNote = await note.save()
+
+   res.json(savedNote)
+    } catch (error){
+        console.error(error.message);
+  res.status(500).send("Internal Server Error Occured");
+
+    }
+
+    
+
+     
+
+  }
+);
+
+module.exports = router;
