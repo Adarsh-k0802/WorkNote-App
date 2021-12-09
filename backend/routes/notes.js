5809b4661abab63220fad0bdf71567e067ac1c07
Adarsh Kumar <adarshk0802@gmail.com>
2021-12-09 09:20:48 +0530

    Update Note Endpoints Created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
 const fetchuser = require("../middleware/fetchuser");
+const { findOneAndUpdate, findById } = require("../models/Note");
 
 //ROUTE 1 Get all the notes using: GET "/api/auth/createuser". login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
@@ -55,5 +56,32 @@ router.post(
 
   }
 );
+
+//ROUTE 3 Update an existing Note using: POST "/api/auth/updatenote". login required
+router.put(
+  "/updatenote/:id",fetchuser, async (req, res) => {
+
+   const {title,description,tag} = req.body;
+   //Create a newNote object
+   const newNote = {};
+   if(title){newNote.title=title};
+   if(description){newNote.description=description};
+   if(tag){newNote.tag=tag};
+
+   //Find the note to be updated
+
+   let note = await Note.findById(req.params.id);
+   if(!note){return res.status(404).send("Not Found")}
+
+   if(note.user.toString() !== req.user.id){
+     return res.status(401).send("Not Allowed");
+   }
+
+   note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote},{new:true})
+   res.json({note});
+
+
+
+  })
 
 module.exports = router;
