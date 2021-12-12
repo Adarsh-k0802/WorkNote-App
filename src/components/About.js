08f123241fa5f1328c81c6bf2e8d5252b38a91bc
Adarsh Kumar <adarshk0802@gmail.com>
2021-12-12 09:55:22 +0530

    useContext hook + useEffect hook

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
-import  React, { useContext } from 'react';
+import  React, { useContext,useEffect } from 'react';
 import noteContext from '../context/notes/noteContext';
 
 const About = () => {
 
     const a = useContext(noteContext)
+    useEffect(() => {
+        // eslint-disable-next-line
+        a.update()
+        
+    }, [])
     return (
         <div>
-           This is about {a.name}
+           This is about {a.state.name} and he is in class {a.state.class}.
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,17 +1,27 @@
 import react from "react";
-
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState=(props)=>{
 
- const state ={
+ const s1 ={
      "name":"adarsh",
      "class":"12b"
+ }
+ const [state, setstate] = useState(s1)
+
+ const update=()=>{
+     setTimeout(() => {
+         setstate({
+            "name":"raju",
+            "class":"10a"
+         })
+     }, 5000);
  }
 
 
 
- return(   <NoteContext.Provider value={state}>
+ return(   <NoteContext.Provider value={{state,update}}>
 
     {props.children}
 
